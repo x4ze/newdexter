12acd34853c5b7d258db5cd029f56f7f3c75dd93
x4ze <0607olle@gmail.com>
2024-09-08 21:45:18 +0200

    added incomplete navbar of version 1

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ import express from "express"
 
 const app = express();
 app.set("view engine", "ejs")
+app.use(express.static('public'))
 
 
 const PORT = process.env.PORT || 3000;
-const IP = process.env.IP
+const IP = process.env.IP || "localhost"
 app.listen(PORT, IP, () => console.log(`listening at ${IP}:${PORT}`));
 
 
 
 app.get("/", async (req, res) => {
-    res.render("login")
+    res.render("1/login")
 })
